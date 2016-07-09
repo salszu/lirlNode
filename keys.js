9545cf28fb6b346b40530923d87455b00889dc90
Salwador Szurlej <sal.szu@gmail.com>
2016-07-09 19:44:21 -0400

    keys.js tokens

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'U6VF7yt2UksJNhEecS3Mdi8Nd',
+  consumer_secret: '7XdQq9KxbeNqSfIc23Nb6spNvoKC31qiZbPQ9DYSk0zrqYy5ug',
+  access_token_key: '750716178700574720-wLsrow33F5RWoM3hkFP8fuJiFYzOSgD',
+  access_token_secret: '37mEi6hD4jbjHjVxzZhqpf1FDX15tMK6xQ5E4akQB47nj',
+}
